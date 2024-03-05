a0ac4a0a95aba864301d1fe89d16c980904a09ae
Ahrav Dutta <ahrav.dutta@trufflesec.com>
2024-03-05 10:00:33 -0800

    add contains and get methods.

--- heap.go
+++ heap.go
@@ -22,6 +22,9 @@
 // - Push: to add new elements to the heap while maintaining the heap property.
 // - Pop: to remove and return the extremal element from the heap.
 // - Peek: to return the extremal element without removing it.
+// - Contains: to check if the heap contains a given element.
+// - Get: to retrieve the first occurrence of an element from the heap.
+// - Remove: to remove an element from the heap and then restore the heap property. (TODO)
 // - Update: to change an element's value and then restore the heap property. (TODO)
 //
 // This package is designed for use cases where a priority queue or any other
@@ -41,6 +44,7 @@ type Heap[T constraints.Ordered] struct {
 	d        int             // Branching factor (number of children per node)
 	heapSize int             // Current size of the heap
 	lessFunc func(T, T) bool // Function to determine order
+	index    map[T][]int     // Hash map to store the indices of each element in the heap
 }
 
 // Option is a type representing configurations for the heap
@@ -50,6 +54,7 @@ type Option[T constraints.Ordered] func(*Heap[T])
 func WithCapacity[T constraints.Ordered](capacity int) Option[T] {
 	return func(h *Heap[T]) {
 		h.data = make([]T, capacity)
+		h.index = make(map[T][]int, capacity)
 	}
 }
 
@@ -61,6 +66,7 @@ func NewHeap[T constraints.Ordered](d int, lessFunc func(T, T) bool, options ...
 		data:     make([]T, 0, defaultCapacity),
 		heapSize: 0,
 		lessFunc: lessFunc,
+		index:    make(map[T][]int, defaultCapacity),
 	}
 
 	for _, option := range options {
@@ -80,22 +86,65 @@ func (h *Heap[T]) child(i, k int) int {
 	return h.d*i + k
 }
 
-// swap swaps the elements at indices i and j.
+// swap swaps the elements at indices i and j and updates the index hash map.
 func (h *Heap[T]) swap(i, j int) {
 	h.data[i], h.data[j] = h.data[j], h.data[i]
+	h.updateIndex(h.data[i], i)
+	h.updateIndex(h.data[j], j)
+}
+
+// updateIndex updates the index hash map for the given element and index.
+func (h *Heap[T]) updateIndex(element T, index int) {
+	indices := h.index[element]
+	// If the element has an index that is the same as the given index, we don't need to update.
+	for _, idx := range indices {
+		if idx == index {
+			break
+		}
+	}
+	h.index[element][0] = index
 }
 
 // Peek returns the minimum element from the heap without removing it.
-func (h *Heap[T]) Peek() T { return h.data[0] }
+func (h *Heap[T]) Peek() T {
+	if h.heapSize == 0 {
+		var zero T
+		return zero
+	}
+	return h.data[0]
+}
+
+// Contains checks if the given element exists in the heap.
+func (h *Heap[T]) Contains(element T) bool {
+	_, exists := h.index[element]
+	return exists
+}
+
+// Get retrieves the element from the heap that matches the given element.
+// If there are duplicates, it returns the first occurrence.
+// If the element is not found, it returns the zero value of type T and false.
+func (h *Heap[T]) Get(element T) (T, bool) {
+	indices, exists := h.index[element]
+	if !exists || len(indices) == 0 {
+		var zero T
+		return zero, false
+	}
+	return h.data[indices[0]], true
+}
 
 // Push adds a new element to the heap.
 func (h *Heap[T]) Push(value T) {
-	if h.heapSize == len(h.data) {
+	if len(h.data) == h.heapSize {
 		h.data = append(h.data, value)
 	} else {
 		h.data[h.heapSize] = value
 	}
 
+	if indices, exists := h.index[value]; exists {
+		h.index[value] = append(indices, indices[0])
+	} else {
+		h.index[value] = []int{h.heapSize}
+	}
 	h.heapSize++
 	h.up(h.heapSize - 1) // Restore heap property after insertion
 }
@@ -106,12 +155,17 @@ func (h *Heap[T]) Pop() T {
 		var zero T
 		return zero
 	}
-
-	minV := h.data[0]
-	h.data[0] = h.data[h.heapSize-1]
+	minValue := h.data[0]
+	lastIndex := h.heapSize - 1
+	h.data[0] = h.data[lastIndex]
+	h.index[minValue] = h.index[minValue][1:] // Remove the first index from the slice of indices
+	if len(h.index[minValue]) == 0 {
+		delete(h.index, minValue) // Remove the element from the index hash map if no more indices
+	}
+	h.swap(0, lastIndex)
 	h.heapSize--
-	h.down(0) // Restore heap property after removal
-	return minV
+	h.down(0)
+	return minValue
 }
 
 // up restores the heap property by bubbling an element up the tree.
--- heap_test.go
+++ heap_test.go
@@ -7,7 +7,7 @@ import (
 	"golang.org/x/exp/constraints"
 )
 
-func TestMinHeapOperations(t *testing.T) {
+func TestHeapOperations(t *testing.T) {
 	type heapOperation[T constraints.Ordered] struct {
 		operation string // "push", "pop", or "peek"
 		value     T      // value to push (ignored for pop and peek)
@@ -18,11 +18,13 @@ func TestMinHeapOperations(t *testing.T) {
 	tests := []struct {
 		name       string
 		d          int
+		isMinHeap  bool
 		operations []heapOperation[int]
 	}{
 		{
-			name: "MinHeap with d=2",
-			d:    2,
+			name:      "MinHeap with d=2",
+			d:         2,
+			isMinHeap: true,
 			operations: []heapOperation[int]{
 				{operation: "push", value: 5},
 				{operation: "push", value: 3},
@@ -36,8 +38,9 @@ func TestMinHeapOperations(t *testing.T) {
 			},
 		},
 		{
-			name: "Single element",
-			d:    2,
+			name:      "MinHeap Single element",
+			d:         2,
+			isMinHeap: true,
 			operations: []heapOperation[int]{
 				{operation: "push", value: 10},
 				{operation: "peek", want: 10},
@@ -45,7 +48,7 @@ func TestMinHeapOperations(t *testing.T) {
 			},
 		},
 		{
-			name: "All elements same",
+			name: "MinHeap All elements same",
 			d:    3,
 			operations: []heapOperation[int]{
 				{operation: "push", value: 1},
@@ -57,8 +60,9 @@ func TestMinHeapOperations(t *testing.T) {
 			},
 		},
 		{
-			name: "Ascending order push",
-			d:    2,
+			name:      "MinHeap Ascending order push",
+			d:         2,
+			isMinHeap: true,
 			operations: []heapOperation[int]{
 				{operation: "push", value: 1},
 				{operation: "push", value: 2},
@@ -68,8 +72,9 @@ func TestMinHeapOperations(t *testing.T) {
 			},
 		},
 		{
-			name: "Descending order push",
-			d:    2,
+			name:      "MinHeap Descending order push",
+			d:         2,
+			isMinHeap: true,
 			operations: []heapOperation[int]{
 				{operation: "push", value: 3},
 				{operation: "push", value: 2},
@@ -79,8 +84,9 @@ func TestMinHeapOperations(t *testing.T) {
 			},
 		},
 		{
-			name: "Higher branching factor",
-			d:    4,
+			name:      "MinHeap Higher branching factor",
+			d:         4,
+			isMinHeap: true,
 			operations: []heapOperation[int]{
 				{operation: "push", value: 5},
 				{operation: "push", value: 3},
@@ -96,70 +102,9 @@ func TestMinHeapOperations(t *testing.T) {
 			},
 		},
 		{
-			name: "Pop from empty heap",
-			d:    2,
-			operations: []heapOperation[int]{
-				{operation: "pop", want: 0, wantHeap: []int{}}, // Assuming default zero value is returned for empty heap
-			},
-		},
-		{
-			name: "Peek into empty heap",
-			d:    2,
-			operations: []heapOperation[int]{
-				{operation: "peek", want: 0}, // Assuming default zero value is returned for empty heap
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		tt := tt
-		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-
-			heap := NewHeap[int](tt.d, func(a, b int) bool { return a < b })
-
-			for _, op := range tt.operations {
-				switch op.operation {
-				case "push":
-					heap.Push(op.value)
-				case "pop":
-					assert.Equal(t, op.want, heap.Pop(), "Pop() returned wrong value")
-				case "peek":
-					assert.Equal(t, op.want, heap.Peek(), "Peek() returned wrong value")
-				default:
-					t.Fatalf("unknown operation: %s", op.operation)
-				}
-
-				if op.wantHeap != nil {
-					for i, wantVal := range op.wantHeap {
-						if i >= heap.heapSize || heap.data[i] != wantVal {
-							t.Errorf("After %s, heap[%d] = %v, want %v", op.operation, i, heap.data[i], wantVal)
-							break
-						}
-					}
-					assert.Equal(t, len(op.wantHeap), heap.heapSize, "heap size not as expected after %s", op.operation)
-				}
-			}
-		})
-	}
-}
-
-func TestMaxHeapOperations(t *testing.T) {
-	type heapOperation[T constraints.Ordered] struct {
-		operation string // "push", "pop", or "peek"
-		value     T      // value to push (ignored for pop and peek)
-		want      T      // expected result for peek or pop
-		wantHeap  []T    // expected heap state after the operation
-	}
-
-	tests := []struct {
-		name       string
-		d          int
-		operations []heapOperation[int]
-	}{
-		{
-			name: "MaxHeap with d=2",
-			d:    2,
+			name:      "MaxHeap with d=2",
+			d:         2,
+			isMinHeap: false,
 			operations: []heapOperation[int]{
 				{operation: "push", value: 5},
 				{operation: "push", value: 3},
@@ -173,7 +118,7 @@ func TestMaxHeapOperations(t *testing.T) {
 			},
 		},
 		{
-			name: "Single element",
+			name: "MaxHeap Single element",
 			d:    2,
 			operations: []heapOperation[int]{
 				{operation: "push", value: 10},
@@ -182,7 +127,7 @@ func TestMaxHeapOperations(t *testing.T) {
 			},
 		},
 		{
-			name: "All elements same",
+			name: "MaxHeap All elements same",
 			d:    3,
 			operations: []heapOperation[int]{
 				{operation: "push", value: 1},
@@ -194,7 +139,7 @@ func TestMaxHeapOperations(t *testing.T) {
 			},
 		},
 		{
-			name: "Ascending order push",
+			name: "MaxHeap Ascending order push",
 			d:    2,
 			operations: []heapOperation[int]{
 				{operation: "push", value: 1},
@@ -205,7 +150,7 @@ func TestMaxHeapOperations(t *testing.T) {
 			},
 		},
 		{
-			name: "Descending order push",
+			name: "MaxHeap Descending order push",
 			d:    2,
 			operations: []heapOperation[int]{
 				{operation: "push", value: 3},
@@ -216,7 +161,7 @@ func TestMaxHeapOperations(t *testing.T) {
 			},
 		},
 		{
-			name: "Higher branching factor",
+			name: "MaxHeap Higher branching factor",
 			d:    4,
 			operations: []heapOperation[int]{
 				{operation: "push", value: 5},
@@ -233,17 +178,17 @@ func TestMaxHeapOperations(t *testing.T) {
 			},
 		},
 		{
-			name: "Pop from empty heap",
+			name: "MaxHeap Pop from empty heap",
 			d:    2,
 			operations: []heapOperation[int]{
-				{operation: "pop", want: 0, wantHeap: []int{}}, // Assuming default zero value is returned for empty heap
+				{operation: "pop", want: 0, wantHeap: []int{}},
 			},
 		},
 		{
-			name: "Peek into empty heap",
+			name: "MaxHeap Peek into empty heap",
 			d:    2,
 			operations: []heapOperation[int]{
-				{operation: "peek", want: 0}, // Assuming default zero value is returned for empty heap
+				{operation: "peek", want: 0},
 			},
 		},
 	}
@@ -253,7 +198,12 @@ func TestMaxHeapOperations(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 
-			heap := NewHeap[int](tt.d, func(a, b int) bool { return a > b })
+			var heap *Heap[int]
+			if tt.isMinHeap {
+				heap = NewHeap[int](tt.d, func(a, b int) bool { return a < b })
+			} else {
+				heap = NewHeap[int](tt.d, func(a, b int) bool { return a > b })
+			}
 
 			for _, op := range tt.operations {
 				switch op.operation {
@@ -268,10 +218,12 @@ func TestMaxHeapOperations(t *testing.T) {
 				}
 
 				if op.wantHeap != nil {
-					for i, wantVal := range op.wantHeap {
-						if i >= heap.heapSize || heap.data[i] != wantVal {
-							t.Errorf("After %s, heap[%d] = %v, want %v", op.operation, i, heap.data[i], wantVal)
-							break
+					if len(op.wantHeap) > 0 {
+						for i, wantVal := range op.wantHeap {
+							if i >= heap.heapSize || heap.data[i] != wantVal {
+								t.Errorf("After %s, heap[%d] = %v, want %v", op.operation, i, heap.data[i], wantVal)
+								break
+							}
 						}
 					}
 					assert.Equal(t, len(op.wantHeap), heap.heapSize, "heap size not as expected after %s", op.operation)
@@ -279,4 +231,56 @@ func TestMaxHeapOperations(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestHeapContains(t *testing.T) {
+	heap := NewHeap[int](2, func(a, b int) bool { return a < b })
+	heap.Push(5)
+	heap.Push(3)
+	heap.Push(4)
+	heap.Push(1)
+	heap.Push(1)
+
+	assert.True(t, heap.Contains(5), "Contains(5) returned false, want true")
+	assert.True(t, heap.Contains(3), "Contains(3) returned false, want true")
+	assert.False(t, heap.Contains(2), "Contains(2) returned true, want false")
+
+	// Ensure duplicates are handled correctly.
+	heap.Pop()
+	assert.True(t, heap.Contains(1), "Contains(5) returned true, want false")
+
+	heap.Pop()
+	assert.False(t, heap.Contains(1), "Contains(1) returned true, want false")
+}
+
+func TestHeapGet(t *testing.T) {
+	heap := NewHeap[int](2, func(a, b int) bool { return a < b })
+	heap.Push(5)
+	heap.Push(3)
+	heap.Push(4)
+	heap.Push(1)
+	heap.Push(1)
+
+	val, ok := heap.Get(5)
+	assert.True(t, ok, "Get(5) returned false, want true")
+	assert.Equal(t, 5, val, "Get(5) returned %d, want 5", val)
+
+	val, ok = heap.Get(3)
+	assert.True(t, ok, "Get(3) returned false, want true")
+	assert.Equal(t, 3, val, "Get(3) returned %d, want 3", val)
+
+	val, ok = heap.Get(2)
+	assert.False(t, ok, "Get(2) returned true, want false")
+	assert.Zero(t, val, "Get(2) returned %d, want 0", val)
+
+	// Ensure duplicates are handled correctly.
+	heap.Pop()
+	val, ok = heap.Get(1)
+	assert.True(t, ok, "Get(1) returned false, want true")
+	assert.Equal(t, 1, val, "Get(1) returned %d, want 1", val)
+
+	heap.Pop()
+	val, ok = heap.Get(1)
+	assert.False(t, ok, "Get(1) returned true, want false")
+	assert.Zero(t, val, "Get(1) returned %d, want 0", val)
 }
